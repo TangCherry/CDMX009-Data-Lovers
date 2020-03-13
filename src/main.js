83a10e36537c765ff4e0e52222453fa7413c8d7b
TangCherry <danaee16@gmail.com>
2020-03-13 11:34:37 -0600

    funcion de todos los pokemons y filtrado listas!

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ kantoPokemons.forEach(poke=>{
 	pokeCard.appendChild(myArticle)
 })  */
 
-/* let cards = document.querySelector(".container3") 
+ let cards = document.querySelector(".container3") 
 
 let pokemonType= document.querySelectorAll('.item-flex');//un variable donde me trae todos los elementos con esa clase
 for(let i=0; i< pokemonType.length; i++){
@@ -32,8 +32,8 @@ for(let i=0; i< pokemonType.length; i++){
 		document.getElementById('first').style.display='none';
 		document.getElementById('typePokemon').style.display='block';
 	let filter = filterByType (pokemonType[i].id);
-		//console.log (pokemonType);
-	});
+		filter.forEach(poke=>{
+		//console.log (filter);
 		let myArticle = document.createElement ("article");
 		let pokemonName = document.createElement ("h2");
 		let number = document.createElement ("td");
@@ -41,20 +41,13 @@ for(let i=0; i< pokemonType.length; i++){
 		let weight = document.createElement ("td");
 		let candyCount = document.createElement ("td");
 		let egg = document.createElement ("td");
-	/*	let nextEvolution = document.createElement ("ul");
 
-			let evolution = pokemonType[i].next_evolution;
-				for (let n=0; n <evolution.length; n++){
-					let listItem = document.createElement("li");
-					listItem.textContent = evolution[n];
-					nextEvolution.appendChild(listItem);
-				} 
-				pokemonName.textContent = pokemonType.name;
-				number.textContent = pokemonType.num;
-				height.textContent ="Height: " + pokemonType.height;
-				weight.textContent ="Weight: " + pokemonType.weight;
-				candyCount.textContent ="Candy count: " + pokemonType.candy_count;
-				egg.textContent = "You can find it in... " + pokemonType.egg;
+				pokemonName.textContent = poke.name;
+				number.textContent = poke.num;
+				height.textContent ="Height: " + poke.height;
+				weight.textContent ="Weight: " + poke.weight;
+				candyCount.textContent ="Candy count: " + poke.candy_count;
+				egg.textContent = "You can find it in... " + poke.egg;
 
 				myArticle.appendChild(pokemonName);
 				myArticle.appendChild(number);
@@ -62,10 +55,12 @@ for(let i=0; i< pokemonType.length; i++){
 				myArticle.appendChild(weight);
 				myArticle.appendChild(candyCount);
 				myArticle.appendChild(egg);
-				//myArticle.appendChild(nextEvolution);
 				cards.appendChild(myArticle);
-}  */
- let cards = document.querySelector(".container3") //tambien se usará este modelo de variable para el filtrado, no lo olvides
+			})
+		});
+	} 
+
+ /* let cards = document.querySelector(".container3") //tambien se usará este modelo de variable para el filtrado, no lo olvides
 
  let pokemonType= document.querySelectorAll('.item-flex');//un variable donde me trae todos los elementos con esa clase
 //let t = "Fire"                        //aquí debe de ir la clase para elegir el id con el evento
@@ -75,4 +70,4 @@ filtrada.forEach(poke=>{
   let article = document.createElement("article")
   article.innerHTML = "<h2>"+poke.name+"</h2>"
   cards.appendChild(article)
-}) 
+}) */
